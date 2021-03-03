c86a4bede92c1a0cd5203e601980081047f43153
Mujaheed0 <60001357+Mujaheed0@users.noreply.github.com>
2021-03-03 20:14:14 +0530

    Update producer-consumer.c

--- day5/producer-consumer.c
+++ day5/producer-consumer.c
@@ -59,7 +59,7 @@ int main()
 
     int a[5] = {1,2,3,4,5}; //Just used for numbering the producer and consumer
 
-    for(int i = 0; i < 1; i++) {
+    for(int i = 0; i < 5; i++) {
         pthread_create(&pro[i], NULL, (void *)producer, (void *)&a[i]);
     }
     for(int i = 0; i < 5; i++) {
